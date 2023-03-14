9148d07548d5d957d2f94e050550fa31d3deae4d
Abdelrahman Elbasel <abdelrahman.elbasel@gmail.com>
2023-03-14 08:06:53 +0200

    Rename Main.tsx to Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 type Props = { children: React.ReactNode };
 
-export const Main = ({ children }: Props) => {
+export const Layout = ({ children }: Props) => {
   return (
     <main className="bg-zinc-900 text-green-400 h-[100vh] h-[100dvh] text-3xl py-8 px-2 overflow-hidden">
       {children}

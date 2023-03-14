2c325d6b8a84fb2b180dce325ecb19177c803da9
Abdelrahman Elbasel <abdelrahman.elbasel@gmail.com>
2023-03-14 09:25:11 +0200

    Add Button.tsx

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type Props = {
+  children: React.ReactNode | string;
+  type?: 'button' | 'submit' | 'reset';
+  className?: string;
+};
+
+export const Button = ({
+  children,
+  type = 'button',
+  className = '',
+}: Props) => {
+  return (
+    <button className={`${styles} ${className}`} type={type}>
+      {children}
+    </button>
+  );
+};
+
+const styles =
+  'text-base border-2 rounded-md p-2 border-green-500 focus:background-green-500 focus:outline-none hover:bg-green-500 hover:cursor-pointer hover:text-blue-900 active:scale-[0.95] transition-all';
